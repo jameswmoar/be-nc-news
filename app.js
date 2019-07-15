2b879ae8d4c760d6025e23aa1522f0d1a8184bd8
James <jameswmoar@gmail.com>
2019-07-15 14:08:24 +0100

    added cors to project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router.js");
+const cors = require('cors')
 
 const {
   handleCustomErrors,
@@ -9,6 +10,7 @@ const {
   sendMethodNotAllowed
 } = require("./errors/index.js");
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/api", apiRouter);
